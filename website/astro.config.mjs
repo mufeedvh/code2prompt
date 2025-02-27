ee69d3da938bd7097fa2e4f00bfb77b872e4b4b5
Olivier D'Ancona <olivier.dancona@master.hes-so.ch>
2025-02-26 18:33:27 -0800

    astro config

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://code2prompt.dev',
 	integrations: [
 		starlight({
 			title: 'Code2prompt',
